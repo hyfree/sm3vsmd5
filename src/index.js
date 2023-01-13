5a73177d85425e6c9a2920242cdb3c19098a446f
hyfree-msi <wangxianqiangcn@foxmail.com>
2023-01-13 21:54:04 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ function mysm3(){
     for (let index = 0; index < 1024*1; index++) {
         s=s+str;
     }
-    console.log(Math.round(new Date().getTime()/1000) )
-    let hashData = sm3(s) // 杂凑
+    var time0=Math.round(new Date().getTime()/1000);
+    console.log("zero="+Math.round(new Date().getTime()/1000) )
+    //let hashData = sm3(s) // 杂凑
+    var bytes = new TextEncoder().encode(s);
+    var sm3Hash = sm3Sum(bytes)
+    var time2=Math.round(new Date().getTime()/1000);
     console.log("sm3="+Math.round(new Date().getTime()/1000) )
     str_md5(s)
     console.log("md5="+Math.round(new Date().getTime()/1000) )
-    console.log(hashData);
-
+    CryptoJS.SHA256(s).toString()
+    console.log("sha256="+Math.round(new Date().getTime()/1000) )
+     console.log(sm3Hash);
 
 }
