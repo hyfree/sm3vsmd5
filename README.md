9b4401684169815cee0613b1a18e7d1253c3381e
hyfree-msi <wangxianqiangcn@foxmail.com>
2023-01-14 10:23:07 +0800

    增加说明

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# sm3vsmd5
-前端加密算法库测试
+# Overview 概述
 
+Compare the performance of md5, sha256 and sm3 in a browser environment
 在浏览器环境下比较md5、sha256和sm3的性能
+
+# How to use 如何使用
+After downloading the js dependency,Open index.html in your browser
+安装js依赖后，使用浏览器打开index.html
+```shell
+pnpm install
+```
+# Test Results 测试结果
+
+```js
+How much time do I need to calculate the hash of 1MB of data:
+index.js:53 
+index.js:54 sm_crypto=14783 ms
+index.js:55 sm3-js=1962 ms
+index.js:56 md5-js=670 ms
+index.js:57 crypto-js_SHA256=823 ms
+index.js:58 Browser_SHA256=1 ms
+```
--- src/index.js
+++ src/index.js
@@ -3,26 +3,59 @@ function mysm3(){
     //const sm3 = require('sm-crypto').sm3
     str="1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
     s="";
-    for (let index = 0; index < 1024*10; index++) {
+    for (let index = 0; index < 1024*1; index++) {
         s=s+str;
     }
-    var time0=Math.round(new Date().getTime()/1000);
-    console.log("zero="+Math.round(new Date().getTime()/1000) )
-    //let hashData = sm3(s) // 杂凑
+    //sm_crypto的SM3实现
+    //https://github.com/JuneAndGreen/sm-crypto
+    var time_sc0=Math.round(new Date().getTime());
+    //console.log("zero="+time0) 
+    let hashData = sm3(s) // 杂凑
+    var time_sc1=Math.round(new Date().getTime());
+    var time_sm_crypto=time_sc1-time_sc0;
+    
+    //sm3-js的SM3实现
+    //https://gitee.com/beginnern/sm3-js
     var bytes = new TextEncoder().encode(s);
+    var time_sj0=Math.round(new Date().getTime())
     var sm3Hash = sm3Sum(bytes)
-    var time2=Math.round(new Date().getTime()/1000);
-    console.log("sm3="+Math.round(new Date().getTime()/1000) )
+    var time_sj1=Math.round(new Date().getTime());
+    var time_sm3js=time_sj1-time_sj0;
+
+    //MD5库
+    //http://pajhome.org.uk/crypt/md5 
+    var time_md5_0=Math.round(new Date().getTime());
     str_md5(s)
-    console.log("md5="+Math.round(new Date().getTime()/1000) )
+    var time_md5_1=Math.round(new Date().getTime());
+    var time_md5_sum=time_md5_1-time_md5_0;
+
+
+    //crypto-js的SHA256
+    //https://github.com/brix/crypto-js
+    var time_cj0=new Date().getTime()
     CryptoJS.SHA256(s).toString()
-    console.log("sha256="+Math.round(new Date().getTime()/1000) )
+    var time_cj1=new Date().getTime()
+    var time_sha256_CryptoJS=time_cj1-time_cj0;
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+    //浏览器接口
+    console.log("sha256="+Math.round(new Date().getTime()) )
     let buf = textToArrayBuffer(s)
     console.log(buf.byteLength);
-    console.log("sha256-原生毫秒="+Math.round(new Date().getTime()) )
-    window.crypto.subtle.digest('SHA-256', buf);//https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
-    console.log("sha256-原生毫秒="+Math.round(new Date().getTime()) )
+    var llq0=Math.round(new Date().getTime());
+    window.crypto.subtle.digest('SHA-256', buf);
+    var llq1=Math.round(new Date().getTime());
+    var time_sha_llq=llq1-llq0;
     console.log(sm3Hash);
+
+    //统计结果
+    console.log("How much time do I need to calculate the hash of 1MB of data:");
+    console.log("")
+    console.log("sm_crypto="+time_sm_crypto+" ms");
+    console.log("sm3-js="+time_sm3js+" ms");
+    console.log("md5-js="+time_md5_sum+" ms");
+    console.log("crypto-js_SHA256="+time_sha256_CryptoJS+" ms");
+    console.log("Browser_SHA256="+time_sha_llq+" ms");
 }
 function textToArrayBuffer(s) {
     var i = s.length;
